64290e1e2aba46f1db157d569b8ab0c3a78a168e
Alys Brooks <alys@gaiwan.co>
2022-01-18 21:00:56 -0600

    Support passing multiple values to spy.

--- src/lambdaisland/glogi.clj
+++ src/lambdaisland/glogi.clj
@@ -44,9 +44,16 @@
 (defmacro finest [& keyvals]
   (log-expr &form :finest keyvals))
 
-(defmacro spy [form]
-  (let [res (gensym)]
-    `(let [~res ~form]
-       ~(log-expr &form :debug [:spy `'~form
-                                :=> res])
-       ~res)))
+(defmacro spy
+  ([form]
+   (let [res (gensym)]
+     `(let [~res ~form]
+        ~(log-expr &form :debug [:spy `'~form
+                                 :=> res])
+        ~res)))
+  ([form & forms]
+   (let [form-res (mapv (fn [f] (let [res (gensym)] (vector f res))) (conj forms form))
+         form-res-dict (into [] (mapcat (fn [[k v]] [k v]) form-res))]
+     `(let ~(into [] (mapcat (fn [[k v]] [v k]) form-res))
+        ~(log-expr &form :debug [:spy form-res-dict])
+        ~(last form-res)))))
