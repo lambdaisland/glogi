756bd7dce0631121d9934189d4278d4165954127
Alys Brooks <alys@gaiwan.co>
2022-02-18 12:10:50 -0600

    Add some tests of glogc using glogi.

--- test/lambdaisland/glogc_test.cljs
+++ test/lambdaisland/glogc_test.cljs
@@ -1,4 +1,3 @@
-
 (ns lambdaisland.glogc-test
   (:require [lambdaisland.glogc :as logc]
             [lambdaisland.glogi :as log]
@@ -7,10 +6,25 @@
 (enable-console-print!)
 
 (deftest smoke-test
-  (let [example-val 5]
+  (let [captured (atom [])
+        handler (fn [record] (swap! captured conj record))
+        example-val 5]
+    (log/add-handler handler)
+    (log/set-levels '{lambdaisland.glogc-test :info})
     (logc/warn :get :these :log :messages)
     (logc/fine :not :you)
     (logc/info :to :show :up :here)
     (is (= example-val (logc/spy example-val)))
-    (is (= :spy1 (logc/spy :spy1 example-val)))))
+    (is (= example-val (logc/spy :spy1 example-val)))
+    (is (= [{:sequenceNumber 0
+             :level :warning
+             :message {:get :these :log :messages :line 14}
+             :logger-name "lambdaisland.glogc-test"
+             :exception nil}
+            {:sequenceNumber 0
+             :level :info
+             :message {:to :show :up :here :line 16}
+             :logger-name "lambdaisland.glogc-test" :exception nil}]
+           (map #(dissoc % :time) @captured)))
+    (log/remove-handler handler)))
 
